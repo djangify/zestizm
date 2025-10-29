e22e215c8094ec380b14c8a7204a73af83d138a4
djangify <djangify@gmail.com>
2025-10-29 14:17:41 +0000

    Add cluster page

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 
 app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("support/", views.support, name="support"),
+    path(
+        "zest-for-life/",
+        TemplateView.as_view(template_name="core/zest-for-life.html"),
+        name="zest-for-life/",
+    ),
     path("robots.txt", views.robots_txt, name="robots_txt"),
 ]
